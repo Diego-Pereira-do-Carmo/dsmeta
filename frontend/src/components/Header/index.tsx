73a58457be97ae698053e173c388ace94b35450f
Diego Pereira do Carmo <carmopereiradiego@gmail.com>
2022-07-13 01:06:11 -0300

    ajuste de desenvolvedor e redirecionamento

--- frontend/src/components/Header/index.tsx
+++ frontend/src/components/Header/index.tsx
@@ -6,9 +6,8 @@ function Header() {
             <div className="dsmeta-logo-container">
                 <img src={logo} alt="DSMeta" />
                 <h1>DSMeta</h1>
-                <p>
-                    Desenvolvido por
-                    <a href="https://www.instagram.com/devsuperior.ig">@devsuperior.ig</a>
+                <p> Desenvovido por <a href="https://www.linkedin.com/in/diego-pereira-do-carmo/"> @Diego Pereira do Carmo
+                </a>
                 </p>
             </div>
         </header>
